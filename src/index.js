f83602a3461768a633a822041b5443140e5812ce
Nikolaev Viktor <maclauddd@gmail.com>
2021-05-04 20:45:00 +0300

    create graphql client

--- src/index.js
+++ src/index.js
@@ -1,14 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+const apolloClient = new ApolloClient({
+  cache: new InMemoryCache(),
+  /**
+   * simple for this example
+   * this need to get from env vars
+   *  */
+  uri: "http://localhost:4000/",
+  connectToDevTools: true,
+});
+
+apolloClient
+  .query({
+    query: gql`
+      {
+        todos {
+          id
+          task
+          complete
+        }
+      }
+    `,
+  })
+  .then((result) => console.log(result));
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
